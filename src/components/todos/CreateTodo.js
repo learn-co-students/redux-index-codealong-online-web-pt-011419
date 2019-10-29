43078ba49c0a6d27fb89c2ce35a037c9314cdf1a
dclements9 <dclements@protonmail.com>
2019-10-29 10:15:49 -0400

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,41 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { connect } from 'react-redux'
-
-class CreateTodo extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-      text: '',
-    };
-  }
-
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.addTodo(this.state)
-  }
-
-  handleChange(event) {
-    this.setState({
-      text: event.target.value
-    });
-  }
-
+import Todo from './Todo'
+ 
+class TodosContainer extends Component {
+ 
+  renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
+ 
   render() {
     return(
       <div>
-        <form onSubmit={this.handleSubmit}>
-    	    <label>add todo</label>
-          <input type="text" onChange={(event) => this.handleChange(event)} value={this.state.text}/>
-          <input type="submit" />
-       </form>
-     </div>
-   );
+        {this.renderTodos()}
+      </div>
+    );
   }
 };
-
-const mapDispatchToProps = dispatch => ({
-  addTodo: formData => dispatch({ type: 'ADD_TODO', payload: formData })
-})
-
-export default connect(null, mapDispatchToProps)(CreateTodo);
+ 
+const mapStateToProps = state => {
+  return {
+    todos: state.todos
+  }
+}
+ 
+export default connect(mapStateToProps)(TodosContainer);
